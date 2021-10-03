043c90fe4064dbfae6b9728a947ed62979752fc2
Rachèl Heimbach <rachel.heimbach@rabobank.nl>
2021-10-03 20:06:15 +0200

    Immutable object fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,7 +33,10 @@ export default function Index() {
         await addItemToCartRequest(id, quantity);
         const updatedItems = items?.map((item) => {
           if (item.id === id) {
-            item.amountInCart = quantity;
+            return {
+              ...item,
+              amountInCart: quantity,
+            };
           }
           return item;
         });
